b94a61caf9c3936749671bb396d60eebc32728ed
khushalyad3 <36162960+khushalyad3@users.noreply.github.com>
2021-09-04 11:12:29 +0530

    again1

---
+++ login_one/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KConstants extends StatefulWidget {
+  @override
+  _KConstantsState createState() => _KConstantsState();
+}
+
+class _KConstantsState extends State<KConstants> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
