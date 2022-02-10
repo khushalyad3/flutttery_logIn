b2c551939582f003eb66f8a2fa7d7f35c7772a4c
Khushal yadav <Khushalyadav90@gmail.com>
2022-02-10 12:45:25 +0530

    changes

---
+++ login_one/lib/Ui_material/kColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class KColors {
+  static const Color ButtonColor = Color(0xFFD81B60);
+  static const Color ErrorIconColor = Color(0xFFE57373);
+  static const Color SuccessIconColor = Color(0xFF81C784);
+  static const Color ToastColor = Color(0xFFFF80AB);
+}
--- login_one/lib/register.dart
+++ login_one/lib/register.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'Ui_material/kColors.dart';
+
 class RegisterUserId extends StatefulWidget {
   @override
   _RegisterUserIdState createState() => _RegisterUserIdState();
@@ -17,149 +19,132 @@ class _RegisterUserIdState extends State<RegisterUserId> {
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
         resizeToAvoidBottomInset: true,
-        body: Form(
-          key: _formKey,
-          child:Stack(
-            children: [
-              // Container(
-              //   height: double.infinity,
-              //   width: double.infinity,
-              //   decoration: BoxDecoration(
-              //     gradient: LinearGradient(
-              //       begin: Alignment.topCenter,
-              //       end: Alignment.bottomCenter,
-              //       colors: [
-              //
-              //
-              //         Color(0xFFFCE4EC),
-              //         Color(0xFFF48FB1),
-              //       ],
-              //       stops: [0.5, 0.9],
-              //     ),
-              //   ),
-              // ),
-              Expanded (
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      child:Column(
-                        children: [
-                          Center(
-                            child: Text('Signup',style:TextStyle(
-                              fontSize: 22,
-                              fontWeight:FontWeight.bold,
-                            )),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left:25.0,right:25),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                suffixIcon: email==0?Icon(Icons.cancel,color: Colors.red[300],):Icon( Icons.check_circle,color: Colors.green[300],),
-                                border: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.grey,
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child:Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Container(
+                  //   height: double.infinity,
+                  //   width: double.infinity,
+                  //   decoration: BoxDecoration(
+                  //     gradient: LinearGradient(
+                  //       begin: Alignment.topCenter,
+                  //       end: Alignment.bottomCenter,
+                  //       colors: [
+                  //
+                  //
+                  //         Color(0xFFFCE4EC),
+                  //         Color(0xFFF48FB1),
+                  //       ],
+                  //       stops: [0.5, 0.9],
+                  //     ),
+                  //   ),
+                  // ),
+                  SizedBox(),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+
+                    child:Column(
+                      children: [
+                        Center(
+                          child: Text('Log In',style:TextStyle(
+                            fontSize: 22,
+                            fontWeight:FontWeight.bold,
+                          )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left:25.0,right:25),
+                          child: TextFormField(
+                            autocorrect: false,
+                            enableSuggestions: false,
+                            textCapitalization: TextCapitalization.none,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              suffixIcon: email==0?Icon(Icons.cancel,color: KColors.ErrorIconColor,):Icon( Icons.check_circle,color:KColors.SuccessIconColor,),
+                              border: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey,
+                                width: 1,),
+                              ),
+                              errorBorder:UnderlineInputBorder(
+                                borderSide: BorderSide(color:KColors.ButtonColor,
                                   width: 1,),
-                                ),
-                                errorBorder:UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.pink[600],
-                                    width: 1,),
-                                ),
                               ),
-                              onChanged: (String value){
-                                Pattern pattern =
-                                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                                regex = new RegExp(pattern);
-                                if(value.isEmpty || value==''){
-                                  email=0;
-                                  return 'Field should not be empty';
-                                }
-                                else if (!regex.hasMatch(value)) {
-                                  email=0;
-                                  return 'Enter Valid Email Id';
-                                }
-                                email=1;
-                                return null;
-                              },
-                              validator: (String value) {
-                                Pattern pattern =
-                                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                                regex = new RegExp(pattern);
-                                if(value.isEmpty || value==''){
-                                  email=0;
-                                  return 'Field should not be empty';
-                                }
-                                else if (!regex.hasMatch(value)) {
-                                  email=0;
-                                  return 'Enter Valid Email Id';
-                                }
-                                email=1;
-                                return null;
-                              },),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left:25.0,right:25),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Password',
-                                suffixIcon: password==0?Icon(Icons.cancel,color: Colors.red[300],):Icon( Icons.check_circle,color: Colors.green[300],),
-                                border: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.grey,
-                                    width: 1,),
-                                ),
-                                errorBorder:UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.pink[600],
-                                    width: 1,),
-                                ),
-                              ),
-                              onChanged: (String value){
-
-                                if(value.isEmpty || value==''){
-                                  password=0;
-                                  return 'Field should not be empty';
-                                }
-                                password=1;
-                                return null;
-                              },
-                              validator: (String value) {
-
-                                if(value.isEmpty || value==''){
-                                  password=0;
-                                  return 'Field should not be empty';
-                                }
-                                password=1;
-                                return null;
-                              },),
-                          ),
-                          SizedBox(height: 20,),
-                          Container(
-                            color: Colors.pink[200],
-                            child: Column(
-                               children: [
-                                 Padding(
-                                   padding: const EdgeInsets.all(8.0),
-                                   child: Text('Password must be 6 characters long',style:TextStyle(
-                                     color: Colors.white,
-                                   )),
-                                 ),
-                               ],
                             ),
-                          ),
-                        ],
-                      ),
+                            onChanged: (String value){
+                              Pattern pattern =
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                              regex = new RegExp(pattern);
+                              if(value.isEmpty || value==''){
+                                email=0;
+                                return 'Field should not be empty';
+                              }
+                              else if (!regex.hasMatch(value)) {
+                                email=0;
+                                return 'Enter Valid Email Id';
+                              }
+                              email=1;
+                              return null;
+                            },
+                            validator: (String value) {
+                              Pattern pattern =
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                              regex = new RegExp(pattern);
+                              if(value.isEmpty || value==''){
+                                email=0;
+                                return 'Field should not be empty';
+                              }
+                              else if (!regex.hasMatch(value)) {
+                                email=0;
+                                return 'Enter Valid Email Id';
+                              }
+                              email=1;
+                              return null;
+                            },),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left:25.0,right:25),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              suffixIcon: password==0?Icon(Icons.cancel,color: Colors.red[300],):Icon( Icons.check_circle,color: Colors.green[300],),
+                              border: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey,
+                                  width: 1,),
+                              ),
+                              errorBorder:UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.pink[600],
+                                  width: 1,),
+                              ),
+                            ),
+                            onChanged: (String value){
+
+                              if(value.isEmpty || value==''){
+                                password=0;
+                                return 'Field should not be empty';
+                              }
+                              password=1;
+                              return null;
+                            },
+                            validator: (String value) {
+
+                              if(value.isEmpty || value==''){
+                                password=0;
+                                return 'Field should not be empty';
+                              }
+                              password=1;
+                              return null;
+                            },),
+                        ),
+                      ],
                     ),
-
-
-                  ],
-                ),
-              ),
-              Align(
-                alignment:AlignmentDirectional.bottomEnd,
-                child: Positioned(
-
-                  child: Padding(
+                  ),
+                  Padding(
                     padding: const EdgeInsets.only(left:25.0,right:25,bottom:15, ),
                     child: SizedBox(
                       width: double.infinity,
@@ -167,12 +152,10 @@ class _RegisterUserIdState extends State<RegisterUserId> {
                       child: ElevatedButton(
                         onPressed: (){
                           if (_formKey.currentState.validate()){
-
                           }
                           else{
                             final snackBar = SnackBar(
-                              backgroundColor: Colors.pink[100],
-
+                              backgroundColor: KColors.ToastColor,
                               content: Text(
                                   'One or more fields have an error. Please check and try again.',style:TextStyle(
                                 color: Colors.black,
@@ -190,7 +173,7 @@ class _RegisterUserIdState extends State<RegisterUserId> {
                           }
                         },
                         style: ElevatedButton.styleFrom(
-                          primary: Colors.pink,
+                          primary: KColors.ButtonColor,
                           onPrimary: Colors.white,
                         ),
                         child: Center(
@@ -199,9 +182,9 @@ class _RegisterUserIdState extends State<RegisterUserId> {
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
 
